e9bd4b09ab3c43bda930dcf9587d8bba2b307925
suresh <suresh.koppisetty@gmail.com>
2014-11-24 18:36:26 -0500

    New user for db and password

--- code/Server/wardrive/wardrive/settings.py
+++ code/Server/wardrive/wardrive/settings.py
@@ -61,8 +61,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'wardrive',
-        'USER': 'dbuser',
-        'PASSWORD': 'wardrive',
+        'USER': 'wardrive',
+        'PASSWORD': 'Watchtower14',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
